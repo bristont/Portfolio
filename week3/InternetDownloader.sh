99b242b9e4322f41d0bcc8e3b4b7a68963ae34e8
bristont <87741951+bristont@users.noreply.github.com>
2021-08-22 07:19:33 -0700

    Add files via upload

--- week3/InternetDownloader.sh
+++ week3/InternetDownloader.sh
@@ -2,6 +2,10 @@
 #Create while loop to download and specify where download is to happen
 
 while read -p "Please type the URL of a file to download or type 'exit' to quit: " url; 
+      if [[ "$url" == "exit" ]]
+      then
+            break
+      fi
       read -p "Type the location of where you would like to download the file to:" file; do 
             wget -O $file.txt $url
 done 
--- week3/filenames.sh
+++ week3/filenames.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-for 
+FILENAME="filenames.txt"
+
+LINES=$(cat $FILENAME)
+
+for LINE in $LINES
+do
+    if [ -d "$LINE" ] ; then
+        echo "$LINE - That’s a directory"
+    elif [ -f "$LINE" ]; then
+        echo "$LINE - That file exists"
+    else
+        echo "$LINE - I don’t know what that is!"
+    fi
+done
