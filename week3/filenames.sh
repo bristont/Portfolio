1d92d2e53639ad708a7d271bfce0f88d40f5db94
bristont <87741951+bristont@users.noreply.github.com>
2021-08-22 17:51:49 -0700

    Add files via upload

--- week3/filenames.sh
+++ week3/filenames.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+# Set parameter for Filename as filenames.txt
 FILENAME="filenames.txt"
-
+# set parameter to read filename.txt
 LINES=$(cat $FILENAME)
 
 for LINE in $LINES
 do
     if [ -d "$LINE" ] ; then
-        echo "$LINE - That’s a directory"
+        echo "$LINE - That’s a directory" #determine if directory
     elif [ -f "$LINE" ]; then
-        echo "$LINE - That file exists"
+        echo "$LINE - That file exists" #if not directory, determine is a file 
     else
-        echo "$LINE - I don’t know what that is!"
+        echo "$LINE - I don’t know what that is!" #other dont know what it is 
     fi
 done
